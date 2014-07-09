a2d1fb4641f74c797f58fc8be9f92aa3d6886888
Allen Hernandez <ahernandez@linkedin.com>
2014-07-08 20:22:53 -0700

    add movie detail page
    	- add movies/detail to routes
    	- add detail template to movies/ under templates/
    	- add link from result to detail page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 	this.route('search', { path: '/search/:query' });
+	this.resource('movies', function() {
+		this.route('detail', { path: '/:movie_id' });
+	});
 });
 
 export default Router;
